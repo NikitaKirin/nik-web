0702526817647433fef96373daca3dce8568a4c1
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-28 12:02:15 +0500

    feat[new blocks]: add contacts block

---
+++ resources/views/pages/mainPage/blocks/contacts.blade.php
@@ -0,0 +1,26 @@
+<section class="container mx-auto w-[1280px] mb-[120px]">
+    <h2 class="block-title"># Контакты</h2>
+    <div class="pl-[20px]">
+        <div class="mb-[40px]">
+            <p><a class="bg-[url('{{ url(asset('images/icons/phone.svg')) }}')] bg-no-repeat bg-left-center pl-[40px]"
+                  href="tel:79822989712">8 (982) 298-97-12</a></p>
+            <p><a style="background-image: url({{asset('images/icons/email.svg')}})"
+                  class="bg-no-repeat bg-left-center pl-[40px]" href="mailto:kirinnikita1406@gmail.com">kirinnikita1406@gmail.com</a>
+            </p>
+        </div>
+        <div class="flex justify-between w-[350px]">
+            <a href="#" class="hover:scale-110">
+                <img class="block" src="{{ asset('images/icons/socialNetworks/github-icon.svg') }}">
+            </a>
+            <a href="#" class="hover:scale-110">
+                <img class="block" src="{{ asset('images/icons/socialNetworks/telegram-icon.svg') }}">
+            </a>
+            <a href="#" class="hover:scale-110">
+                <img class="block" src="{{ asset('images/icons/socialNetworks/vk-icon.svg') }}">
+            </a>
+            <a href="#" class="hover:scale-110">
+                <img class="block" src="{{ asset('images/icons/socialNetworks/whatsapp-icon.svg') }}">
+            </a>
+        </div>
+    </div>
+</section>
--- resources/views/pages/mainPage/main.blade.php
+++ resources/views/pages/mainPage/main.blade.php
@@ -8,5 +8,6 @@
         @include('pages.mainPage.blocks.skills')
         @include('pages.mainPage.blocks.workExperience')
         @include('pages.mainPage.blocks.projects')
+        @include('pages.mainPage.blocks.contacts')
     </main>
 @endsection
