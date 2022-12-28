589d6d60736c4a4a5b7d49b859e7fcfed1bf918e
NikitaKirin <kirinnikita1406@gmail.com>
2022-12-28 12:11:08 +0500

    feat[contacts]: add margin bottom

--- resources/views/pages/mainPage/blocks/contacts.blade.php
+++ resources/views/pages/mainPage/blocks/contacts.blade.php
@@ -2,7 +2,7 @@
     <h2 class="block-title"># Контакты</h2>
     <div class="pl-[20px]">
         <div class="mb-[40px]">
-            <p><a class="bg-[url('{{ url(asset('images/icons/phone.svg')) }}')] bg-no-repeat bg-left-center pl-[40px]"
+            <p class="mb-[25px]"><a class="bg-[url('{{ url(asset('images/icons/phone.svg')) }}')] bg-no-repeat bg-left-center pl-[40px]"
                   href="tel:79822989712">8 (982) 298-97-12</a></p>
             <p><a style="background-image: url({{asset('images/icons/email.svg')}})"
                   class="bg-no-repeat bg-left-center pl-[40px]" href="mailto:kirinnikita1406@gmail.com">kirinnikita1406@gmail.com</a>
