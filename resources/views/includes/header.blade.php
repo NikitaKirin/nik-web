3a4241d471a953bd80405e18409e76dcf0d179ab
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-05 14:06:17 +0500

    fix[adapting for mobile]: header for 320 px

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,11 @@
 <header class="mb-[60px] lg:px-[50px] lg:pt-[30px] lg:mb-[160px]">
     <div class="container max-w-7xl mx-auto">
-        <div class="border-b-2 py-[20px] px-[15px] bg-white flex items-center justify-between fixed w-full lg:pb-[30px] lg:mb-[90px]">
+        <div class="border-b-2 py-[20px] px-[5px] bg-white z-10 flex items-center justify-between fixed w-full lg:pb-[30px] lg:mb-[90px]">
             <p class="logo text-[25px] text-center font-bold lg:text-[32px] lg:w-1/3">
                 <a href="#">&lt;nik-web&gt;</a>
             </p>
             <div class="flex justify-end">
-                <nav class="menu-nav p-0 text-[18px] list-none mr-[10px] lg:text-[18px] lg:uppercase lg:justify-between lg:w-2/3">
+                <nav class="menu-nav p-0 text-[18px] list-none mr-[5px] lg:text-[18px] lg:uppercase lg:justify-between lg:w-2/3">
                     <li class="hover:opacity-50 active:opacity-50"><a href="#">Обо мне</a></li>
                     <li class="hover:opacity-50 active:opacity-50">Мои навыки</li>
                     <li class="hover:opacity-50 active:opacity-50">Проекты</li>
@@ -17,8 +17,8 @@
                 </a>
             </div>
         </div>
-        <div class="flex items-center justify-between flex-col gap-[35px] mb-[60px">
-            <img src="{{ asset('images/promo-image.jpg') }}" alt="Промо изображение" class="block w-[100%] mt-[110px]">
+        <div class="flex items-center justify-between flex-col gap-[35px] mb-[60px]">
+            <img src="{{ asset('images/promo-image.jpg') }}" alt="Промо изображение" class="block w-[100%] mt-[130px]">
             <h1 class="text-[30px] uppercase font-bold leading-[40px] text-center">Кирин Никита <br> Junior
                 php-developer</h1>
         </div>
