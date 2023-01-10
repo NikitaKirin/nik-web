3c6abf4747a090766599a516ab03c552b867db62
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-10 17:07:35 +0500

    feat[branding]: add brand

---
+++ resources/views/brand/footer.blade.php
@@ -0,0 +1,4 @@
+<p class="small m-n">
+    © Copyright {{date('Y')}}
+    <a href="{{ env('app_url') }}" target="_blank">Nik-Web</a>
+</p>
---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,14 @@
+@push('head')
+    <link
+            href="{{ asset('favicon.ico') }}"
+            id="favicon"
+            rel="icon"
+    >
+@endpush
+
+<p class="h2 n-m font-thin v-center">
+    <img src="{{ asset('favicon.ico') }}" width="50px" style="margin-right: 10px;">
+    <span class="m-l d-none d-sm-block">
+        Nik-Web
+    </span>
+</p>
