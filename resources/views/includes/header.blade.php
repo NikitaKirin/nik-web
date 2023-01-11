2bccbc234b4708992bd333b462766ab7a9781910
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-11 19:24:36 +0500

    feat[headerMenu]: add logo-link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container max-w-7xl mx-auto">
         <div class="main-menu max-w-screen-xl border-b-2 py-[20px] px-[5px] bg-white z-10 flex items-center justify-between fixed top-0 w-full sm:px-[25px] lg:py-[30px] min-[1310px]:px-0">
             <p class="logo text-[25px] text-center font-bold lg:text-[32px] lg:w-1/3 lg:text-start">
-                <a href="#">&lt;nik-web&gt;</a>
+                <a href="/">&lt;nik-web&gt;</a>
             </p>
             <div class="flex justify-end lg:justify-between lg:block lg:w-2/3">
                 <nav class="menu-nav p-0 text-[18px] list-none mr-[5px] uppercase min-[425px]:mr-[35px] lg:text-[18px] lg:flex lg:justify-between lg:opacity-100 lg:h-full lg:transform-none lg:mr-0">
