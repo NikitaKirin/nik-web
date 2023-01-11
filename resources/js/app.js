aea89bba2f62336d67468db5b3680e0f3bf8e5a0
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-11 19:23:21 +0500

    feat[headerMenu]: add smooth scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 import {menuButton} from "./mainPage/menuButton";
 import {popUp} from "./mainPage/popUp";
+import {scroll} from "./mainPage/scroll";
 
 menuButton();
 popUp();
+scroll();
