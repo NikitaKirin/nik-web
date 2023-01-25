38e9a48324af2c790c312fa64a0be466f1e240d7
NikitaKirin <kirinnikita1406@gmail.com>
2023-01-25 12:25:34 +0500

    fix: flex-container for projects-cards and their row-gaps

--- resources/views/pages/mainPage/blocks/projects.blade.php
+++ resources/views/pages/mainPage/blocks/projects.blade.php
@@ -1,6 +1,6 @@
 <section class="mb-[60px] sm:mb-[80px] lg:mb-[120px]" id="projects">
     <h2 class="block-title"># Реализованные проекты</h2>
-    <div class="flex justify-start flex-wrap gap-[15px] lg:px-[15px]">
+    <div class="flex justify-between flex-wrap lg:px-[15px]" style="row-gap: 15px">
         @foreach($projects as $project)
             <x-project-card :$project :$loop/>
         @endforeach
